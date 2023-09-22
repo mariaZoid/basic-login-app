650be354662e4869287a1edb827c14f23571544b
Maria P <maria@zoidpay.com>
2023-09-23 00:38:01 +0300

    setup basic express

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,22 @@
-const app = () => {
- console.log("aloha")
-}
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-app()
+(async () => {
+  try {
+    const app = express();
+    // setup config
+    require('dotenv').config();
+
+    app.use(cors());
+    
+    const port = process.env.PORT;
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    app.listen(port, () => console.log(`App listening on the port ${port}`));
+  } catch (err) {
+    console.log("Error on bootstrapping the application: %O", err);
+  }
+})();
